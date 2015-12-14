1597f73c8d012c24205317ba75e4a80a0522c44e
Parasher Ghimire <prs.cursivevoice@gmail.com>
2015-12-13 23:48:16 -0600

    Uploaded app on github

--- bookhandler.py
+++ bookhandler.py
@@ -1,8 +1,59 @@
 import webapp2
-import main
-class BookHandler(main.Handler):
+import urllib2
+import base
+import os
+import json
+import db
+class BookHandler(base.Handler):
+    '''Handles all the request involving generating books from Google Book Database and adding to user list'''
+
+    def get(self):
+        if self.user:
+        	self.render("start.html", userperson = self.user.username)
+
     def post(self):
-        userdata=self.requset.get();
-        
-        
-    
+        if(self.request.get("book")):
+            base_url="https://www.googleapis.com/books/v1/volumes?q="
+            query=self.request.get("field-keywords")
+            #sends request to Google Book API to get books from user requested title
+            try:
+                content = urllib2.urlopen(base_url+query).read()
+            except:
+                return
+            if content:
+                js = json.loads(content)
+                items = self.retrieve_summary(js)
+                self.render("start.html", userperson=self.user.username, items=items)
+        else:
+            bookInfo = self.request.get('postvar')
+            books = json.load(bookInfo)
+            for book in books:
+                booktitle = book
+                author = books[book][0]
+                imageurl = books[book][1]
+                iswish = books[book][2]
+                self._update_match(booktitle, iswish)
+                bookdb = db.user_book(booktitle=booktitle, author=author,username=self.user.username,iswish=iswish)
+                bookdb.put()
+
+    def _update_match(self, booktitle, iswish):
+        matches = self.check_match(booktitle, iswish)
+        for e in matches[1]:
+            if ret[0]:
+                match = db.user_match(user_wish=self.user.username, user_have= e.username, booktitle=e.booktitle)
+                match.put()
+            else: #if it is for user wishlist
+                match = db.user_match(user_have=self.user.username, user_wish= e.username, booktitle=e.booktitle)
+                match.put() 
+
+    def retrieve_summary(self, json_data):
+        items = []
+        for item in json_data['items']:
+            title = item['volumeInfo']['title']
+            img_url = item['volumeInfo']['imageLinks']['thumbnail']
+            authors = item['volumeInfo']['authors'] if 'authors' in item['volumeInfo'] else ""
+            authors = ', '.join(authors)
+            description = item['volumeInfo']['description'][:300] if 'description' in item['volumeInfo']  else ""
+
+            items.append([title,authors, img_url, description])
+        return items
--- db.py
+++ db.py
@@ -3,20 +3,27 @@ import hashlib
 import random
 from string import letters
 #USER ACCOUNT DATABASE/ handles userinfo,login,logouts
+#methods to hash and validate the password
 def gen_rand():
     length=5
     return ''.join(random.choice(letters) for x in xrange(length))
+
 def gen_hash_pw(name,pw,salt=None):
     if not salt:
         salt=gen_rand()
     hashp=hashlib.sha256(name+pw+salt).hexdigest()
     return '%s,%s'%(salt,hashp)
+
 def users_key(group='default'):
     return db.Key.from_path('users',group)
+
 def valid_pw(name,password,h):
     salt=h.split(',')[0]
     return h==gen_hash_pw(name,password,salt)
-class user_acc(db.Model):               
+
+class user_acc(db.Model):
+    '''User account datastore object'''
+
     username=db.StringProperty(required=True)
     password=db.StringProperty(required=True)
     email=db.StringProperty(required=True)
@@ -25,21 +32,27 @@ class user_acc(db.Model):
     @classmethod
     def by_id(cls,uid):
         return user_acc.get_by_id(uid,parent=None)
+
     @classmethod
     def by_name(cls,name):
         #u=db.GqlQuery("SELECT * FROM user_acc WHERE username='"+name+"'")
         u=user_acc.all().filter('username =',name).get()
         return u
+
     @classmethod
     def register(cls,username,password,email):
-        password_protected=gen_hash_pw(username,password)
-        return user_acc(username=username,password=password_protected,email=email)
+        password_protected=gen_hash_pw(username, password)
+        return user_acc(username=username, password=password_protected, email=email)
+
     @classmethod
     def login(cls,name,pw):
         user=cls.by_name(name)
         if user and valid_pw(name,pw,user.password):
             return user
+
 class user_book(db.Model):
+    '''User book records datastore object'''
+
     username=db.StringProperty()
     booktitle=db.StringProperty(required=True)
     isbn=db.StringProperty()
@@ -54,15 +67,20 @@ class user_book(db.Model):
     @classmethod
     def get_all(cls,name):
         return db.GqlQuery("SELECT * FROM user_book WHERE username='"+name+"'")
+
     @classmethod
     def get_by_query(cls,query):
         return db.GqlQuery("SELECT * FROM user_book WHERE booktitle='"+query+"'")
+
     @classmethod
     def check_by_name(cls,bookname,iswish):
         return db.GqlQuery("SELECT * FROM user_book WHERE booktitle='"+bookname+"'")
+
     
     
 class user_match(db.Model):
+    '''Currently Matched users datastore object'''
+
     user_wish=db.StringProperty()
     user_have=db.StringProperty()
     booktitle=db.StringProperty()
